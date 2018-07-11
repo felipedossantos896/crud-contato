b634a08f6af553fc5ef11755bed9cba4e832033d
felipe <felipe@GS-EXP-05.GESTSING.BR>
2018-07-11 13:01:27 -0300

    Implementando a criação de contatos e pesquisa.

--- src/main/java/br/com/fellps/contato/controller/ContatoController.java
+++ src/main/java/br/com/fellps/contato/controller/ContatoController.java
@@ -1,20 +1,50 @@
 package br.com.fellps.contato.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.fellps.contato.model.Contato;
+import br.com.fellps.contato.model.Status;
+import br.com.fellps.contato.service.ContatoService;
 
 @Controller
 @RequestMapping("/contatos")
 public class ContatoController {
+	
+	@Autowired
+	private ContatoService contatoService;
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("CadastroContato");
 		mv.addObject(new Contato());
 		return mv;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Contato contato) {
+		System.out.println("Salvando....." + contato.getNome());
+		contatoService.salvar(contato);
+		return "redirect:/contatos/novo";
+	}
+	
+	@RequestMapping
+	public ModelAndView pesquisar() {
+		ModelAndView mv = new ModelAndView("PesquisaContato");
+		mv.addObject("contatos", contatoService.listar());
+		return mv;
+	}
+	
+	@ModelAttribute("todosStatus")
+	public List<Status> todosStatus() {
+		return Arrays.asList(Status.values());
 	}
 	
 }
--- src/main/java/br/com/fellps/contato/service/ContatoService.java
+++ src/main/java/br/com/fellps/contato/service/ContatoService.java
@@ -1,8 +1,25 @@
 package br.com.fellps.contato.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.fellps.contato.model.Contato;
+import br.com.fellps.contato.repository.Contatos;
 
 @Service
 public class ContatoService {
 
+	@Autowired
+	private Contatos contatos;
+	
+	public void salvar(Contato contato) {
+		contatos.save(contato);
+	}
+	
+	public List<Contato> listar() {
+		return contatos.findAll();
+	}
+	
 }
